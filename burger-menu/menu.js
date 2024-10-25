f205a02679f2ad611ee5068a328052d02528d94d
annakvern <anna.kvernplassen@gmail.com>
2024-10-25 15:36:13 +0200

    add comments

--- burger-menu/menu.js
+++ burger-menu/menu.js
@@ -1,11 +1,12 @@
 
 window.addEventListener('DOMContentLoaded', main); 
 
+/** Start of the program */
 function main() {
   menu.onclick = menuToggle;
   console.log('Inne i main');
 }
-
+/** Toggle the menu by checking if it's already showing or not. */
 function menuToggle() {
   var toggle = document.getElementById("mobile-links");
   if (toggle.style.display === "block") {
